371904e01a7df550aa20f8dac36d53fd2ed62a7f
lafar6502 <rafal.gwizdala@gmail.com>
2015-11-19 16:07:58 +0100

    save

---
+++ NGinnBPM.MessageBus.Azure/AzureMessageQueues.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NGinnBPM.MessageBus.Impl;
+using Microsoft.ServiceBus.Messaging;
+using System.IO;
+
+namespace NGinnBPM.MessageBus.Azure
+{
+    public class AzureMessageQueues
+    {
+        public static void SendMessageBatch(IEnumerable<MessageContainer> messages, string destinationQueue)
+        {
+            
+        }
+
+        public static BrokeredMessage Package(MessageContainer mc)
+        {
+            var bm = new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(mc.BodyStr)), true);
+            bm.Label = mc.Label;
+            bm.CorrelationId = mc.CorrelationId;
+            bm.ContentType = "text/json";
+            if (mc.DeliverAt > DateTime.Now)
+            {
+                bm.ScheduledEnqueueTimeUtc = mc.DeliverAt.ToUniversalTime();
+            }
+            if (mc.HasHeader(MessageContainer.HDR_TTL))
+            {
+                var ttl = mc.GetDateTimeHeader(MessageContainer.HDR_TTL, DateTime.MaxValue);
+                if (ttl > DateTime.Now) bm.TimeToLive = ttl - DateTime.Now;
+            }
+            return bm;
+        }
+
+
+    }
+}
---
+++ NGinnBPM.MessageBus.Azure/AzureMessageTransport.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NGinnBPM.MessageBus.Impl;
+
+namespace NGinnBPM.MessageBus.Azure
+{
+    class AzureMessageTransport : IMessageTransport, IStartableService
+    {
+        public string Endpoint
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Send(MessageContainer message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendBatch(IList<MessageContainer> messages, object conn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event MessageArrived OnMessageArrived;
+
+        public event MessageArrived OnMessageToUnknownDestination;
+
+        public MessageContainer CurrentMessage
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void ProcessCurrentMessageLater(DateTime howLater)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsRunning
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
